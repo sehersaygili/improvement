9d435f68318be88c4d2d71a58c1df20080f87be4
sehersaygili <77735385+sehersaygili@users.noreply.github.com>
2023-01-01 14:56:59 +0300

    Change Design Home Page #4
    Header and Footer Updated

--- improvement/resources/views/layouts/app.blade.php
+++ improvement/resources/views/layouts/app.blade.php
@@ -5,34 +5,86 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Small Business - Start Bootstrap Template</title>
-        <!-- Favicon-->
+        <title>Seher's Ecom Site</title>
         <link rel="icon" type="image/x-icon" href="{{asset('assets/assets/favicon.ico')}}" />
-        <!-- Core theme CSS (includes Bootstrap)-->
         <link href="{{asset('assets/css/styles.css') }}" rel="stylesheet" />
         @livewireStyles
     </head>
     <body>
-        <!-- Responsive navbar-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container px-5">
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Services</a></li>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-3"> 
+                <div class="collapse navbar-collapse" id="navbarLeftAlignExample">
+                    <ul class="navbar-nav me-auto mb-0 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">My account</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Featured products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">WishList</a>
+                        </li>
                     </ul>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-0 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#!">Order Tracking</a></li>
+                    </ul>  
                 </div>
             </div>
         </nav>
         {{ $slot }}
-        <!-- Footer-->
-        <footer class="py-5 bg-dark">
-            <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p></div>
+        <footer class="bg-light text-center text-white">
+            <div class="container p-4 pb-4">
+                <!-- Twitter -->
+                <a
+                    class="btn text-white rounded-circle m-1"
+                    style="background-color: #ccc;"
+                    href="#!"
+                    role="button"
+                    ><i class="fab fa-twitter"></i
+                ></a>
+
+                <!-- Google -->
+                <a
+                    class="btn text-white rounded-circle m-1"
+                    style="background-color: #ccc;"
+                    href="#!"
+                    role="button"
+                    ><i class="fab fa-google"></i
+                ></a>
+
+                <!-- Instagram -->
+                <a
+                    class="btn text-white rounded-circle m-1"
+                    style="background-color: #ccc;"
+                    href="#!"
+                    role="button"
+                    ><i class="fab fa-instagram"></i
+                ></a>
+
+                <!-- Linkedin -->
+                <a
+                    class="btn text-white rounded-circle m-1"
+                    style="background-color: #ccc;"
+                    href="#!"
+                    role="button"
+                    ><i class="fab fa-linkedin-in"></i
+                ></a>
+                <!-- Github -->
+                <a
+                    class="btn text-white rounded-circle m-1"
+                    style="background-color: #ccc;"
+                    href="#!"
+                    role="button"
+                    ><i class="fab fa-github"></i
+                ></a>
+            </div>
         </footer>
+        <script src="https://kit.fontawesome.com/32eb9ba2f5.js" crossorigin="anonymous"></script>
         <!-- Bootstrap core JS-->
         <script src="{{asset('assets/https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js')}}"></script>
         <!-- Core theme JS-->
