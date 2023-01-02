dbb6739cdcc6fdb10be9180cb79d268182512b7e
sehersaygili <77735385+sehersaygili@users.noreply.github.com>
2023-01-02 18:00:33 +0300

    Change Design Home Page #4

--- improvement/resources/views/layouts/app.blade.php
+++ improvement/resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
         <title>Seher's Ecom Site</title>
         <link rel="icon" type="image/x-icon" href="{{asset('assets/assets/favicon.ico')}}" />
         <link href="{{asset('assets/css/styles.css') }}" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         @livewireStyles
     </head>
     <body>
@@ -86,9 +87,12 @@
         </footer>
         <script src="https://kit.fontawesome.com/32eb9ba2f5.js" crossorigin="anonymous"></script>
         <!-- Bootstrap core JS-->
-        <script src="{{asset('assets/https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="{{asset('assets/js/scripts.js')}}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
         @livewireScripts
     </body>
 </html>
--- improvement/resources/views/livewire/home-component.blade.php
+++ improvement/resources/views/livewire/home-component.blade.php
@@ -1,45 +1,115 @@
-<!-- Page Content-->
 <div class="container px-4 px-lg-5">
             <!-- Heading Row-->
             <div class="row gx-4 gx-lg-5 align-items-center my-5">
-                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="https://dummyimage.com/900x400/dee2e6/6c757d.jpg" alt="..." /></div>
-                <div class="col-lg-5">
-                    <h1 class="font-weight-light">Business Name or Tagline</h1>
-                    <p>This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
-                    <a class="btn btn-primary" href="#!">Call to Action!</a>
+                <div class="col-lg-3">
+                    <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+                        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                        <span class="fs-7">All Departments</span>
+                        </a><hr>
+                        <ul class="nav nav-pills flex-column mb-auto">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">Cell Phones</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">Computers & Accessories</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">Television & Video</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">Smartwatches</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">Headphones</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">Camera & Photo</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">Video Games</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">Sports & Outdoors</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">Best Sellers</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">Top 100 Offers</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" aria-current="page">New Arrivals</a>
+                        </li>
+                    </div>
                 </div>
-            </div>
-            <!-- Call to Action-->
-            <div class="card text-white bg-secondary my-5 py-4 text-center">
-                <div class="card-body"><p class="text-white m-0">This call to action card is a great place to showcase some important information or display a clever tagline!</p></div>
-            </div>
+                <div class="col-lg-9">
+                        <nav class="navbar navbar-expand-lg navbar-light ">
+                            <div class="container px-3"> 
+                                <div class="collapse navbar-collapse" id="navbarLeftAlignExample">
+                                    <ul class="navbar-nav me-auto mb-0 mb-lg-0 pb-5">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Today's Deal</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Renewed Deals</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Outlet</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Coupons</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                    <ul class="navbar-nav ms-auto mb-0 mb-lg-0 pb-5">
+                                        <li class="nav-item"><a class="nav-link" href="#!">Super Discount</a></li>
+                                    </ul>  
+                                </div>
+                            </div>
+                        </nav>
+                    <img class="img-fluid rounded mb-4 mb-lg-0" src="{{asset('assets/images/home.jpg')}}" alt="..." />
+                    <div class="text-center text-white">
+                        <!-- Carousel -->
+                    <div>
+                        <marquee behavior="scroll" scrollamount="10" class="pt-4">
+                            <img src="{{asset('assets/images/1.png')}}"  alt=".." style="width:100px;" class="pt-3">
+                            <img src="{{asset('assets/images/2.png')}}"  alt=".." style="width:100px;" class="pt-3">
+                            <img src="{{asset('assets/images/3.png')}}"  alt=".." style="width:100px;" class="pt-3">
+                            <img src="{{asset('assets/images/4.png')}}"  alt=".." style="width:100px;" class="pt-3">
+                            <img src="{{asset('assets/images/5.png')}}"  alt=".." style="width:100px;" class="pt-3">
+                            <img src="{{asset('assets/images/6.png')}}"  alt=".." style="width:100px;" class="pt-3">
+                        </marquee>
+
+                        </div>  
+                    </div>
+                </div>
+            </div>           
             <!-- Content Row-->
             <div class="row gx-4 gx-lg-5">
                 <div class="col-md-4 mb-5">
                     <div class="card h-100">
                         <div class="card-body">
-                            <h2 class="card-title">Card One</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
+                            <img src="{{asset('assets/images/echo.jpeg')}}"  alt=".." style="width:100%;" >
+                            <h6 class="card-title"><i class="fa-solid fa-right-long"></i>  Home Speaker</h6>
                         </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
+                    </div>
+                </div>
+               
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <img src="{{asset('assets/images/echo.jpeg')}}"  alt=".." style="width:100%;" >
+                            <h6 class="card-title"><i class="fa-solid fa-right-long"></i>  Home Speaker</h6>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-4 mb-5">
                     <div class="card h-100">
                         <div class="card-body">
-                            <h2 class="card-title">Card Two</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
+                            <img src="{{asset('assets/images/echo.jpeg')}}"  alt=".." style="width:100%;" >
+                            <h6 class="card-title"><i class="fa-solid fa-right-long"></i>  Home Speaker</h6>
                         </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-5">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h2 class="card-title">Card Three</h2>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem magni quas ex numquam, maxime minus quam molestias corporis quod, ea minima accusamus.</p>
-                        </div>
-                        <div class="card-footer"><a class="btn btn-primary btn-sm" href="#!">More Info</a></div>
                     </div>
                 </div>
             </div>
